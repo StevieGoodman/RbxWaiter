b08f5b0ef50c6fa23a03dcf50428f62b2385038b
Stevie Goodman <mail@steviegoodman.com>
2024-01-22 05:50:58 +0000

    Fixed rull reference error

--- src/init.lua
+++ src/init.lua
@@ -1,8 +1,6 @@
 local waiter = {
     get = require(script.get),
     collect = require(script.collect),
-    waitFor = require(script.waitFor),
-    waitCollect = require(script.waitCollect),
 }
 
 return waiter
